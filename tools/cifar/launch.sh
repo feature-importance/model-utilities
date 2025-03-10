2e9bc3e0a64a27137cd9d090d6389fbe41fe8e64
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2025-03-10 19:13:56 +0000

    training scripts for slurm

--- tools/cifar/launch.sh
+++ tools/cifar/launch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
-#SBATCH -p swarm_h100,swarm_a100
+#SBATCH -p swarm_h100,swarm_a100,a100,scavenger_mathsa100,swarm_l4,scavenger_4a100,scavenger_8h100
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH -c 32
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 
 module load conda/python3
 source activate torchwatcher
--- tools/cifar/train_all.sh
+++ tools/cifar/train_all.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-for SEED in $(seq 1 3)
+for SEED in $(seq 0 2)
 do
   for DATASET in cifar10 cifar100
   do
-    for MODEL in resnet18_3x3 resnet34_3x3 resnet50_3x3 resnet101_3x3 resnet152_3x3
-    do
-      sbatch launch.sh --dataset "$DATASET" --model "$MODEL" --seed "$SEED"
-    done
-    for MODEL in resnet18 resnet34 resnet50 resnet101 resnet152
+    for MODEL in resnet18_3x3 resnet34_3x3 resnet50_3x3 resnet101_3x3 resnet152_3x3 resnet18 resnet34 resnet50 resnet101 resnet152
     do
       sbatch launch.sh --dataset "$DATASET" --model "$MODEL" --seed "$SEED"
     done
