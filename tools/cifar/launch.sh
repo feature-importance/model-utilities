c6d311bda881126e5a9f93500ba3c2e73db24f0d
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2025-03-10 19:20:44 +0000

    training scripts for slurm

--- tools/cifar/launch.sh
+++ tools/cifar/launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-#SBATCH -p swarm_h100,swarm_a100,a100,scavenger_mathsa100,swarm_l4,scavenger_4a100,scavenger_8h100
-#SBATCH --mem=64G
+#SBATCH -p swarm_h100,swarm_a100,a100,scavenger_mathsa100,swarm_l4
+#SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
-#SBATCH -c 32
+#SBATCH -c 20
 #SBATCH --time=03:00:00
 
 module load conda/python3
