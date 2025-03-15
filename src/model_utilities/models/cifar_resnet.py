99e2b0fc8f66d66bc0e9515513396b18a1bc71f2
Jonathon Hare <jsh2@ecs.soton.ac.uk>
2025-03-15 11:09:48 +0000

    prepping pretrained models

--- src/model_utilities/models/cifar_resnet.py
+++ src/model_utilities/models/cifar_resnet.py
@@ -5,16 +5,16 @@ from torchvision.models import (resnet18, resnet34, resnet50, resnet101,
 from ..transforms._cifar_presets import ImageClassificationEval
 
 __all__ = [
-    "resnet18_3x3", "ResNet18_3x3_Weights",
-    "resnet34_3x3", "ResNet34_3x3_Weights",
-    "resnet50_3x3", "ResNet50_3x3_Weights",
-    "resnet101_3x3", "ResNet101_3x3_Weights",
-    "resnet152_3x3", "ResNet152_3x3_Weights",
-    "ResNet18_CIFAR_Weights",
-    "ResNet34_CIFAR_Weights",
-    "ResNet50_CIFAR_Weights",
-    "ResNet101_CIFAR_Weights",
-    "ResNet152_CIFAR_Weights"
+    "resnet18_3x3", #"ResNet18_3x3_Weights",
+    "resnet34_3x3", #"ResNet34_3x3_Weights",
+    "resnet50_3x3", #"ResNet50_3x3_Weights",
+    "resnet101_3x3", #"ResNet101_3x3_Weights",
+    "resnet152_3x3", #"ResNet152_3x3_Weights",
+    #"ResNet18_CIFAR_Weights",
+    #"ResNet34_CIFAR_Weights",
+    #"ResNet50_CIFAR_Weights",
+    #"ResNet101_CIFAR_Weights",
+    #"ResNet152_CIFAR_Weights"
 ]
 
 def _make_resnet_3x3(net):
