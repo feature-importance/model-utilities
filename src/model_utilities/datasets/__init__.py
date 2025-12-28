ac426b0d99f3af9433812eb0aa2021e117acf817
Jonathon Hare <jsh2@soton.ac.uk>
2025-12-28 13:27:13 +0000

    add imnet subsets

--- src/model_utilities/datasets/__init__.py
+++ src/model_utilities/datasets/__init__.py
@@ -1,2 +1,2 @@
-from imagenet_hdf5 import ImageNetHDF5
-from imagenet_subsets_hdf5 import ImageNet50HDF5, ImageNet100HDF5
+from .imagenet_hdf5 import ImageNetHDF5
+from .imagenet_subsets_hdf5 import ImageNet50HDF5, ImageNet100HDF5
