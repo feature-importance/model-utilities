9f18f2015dcacae122beb64fe96aed931f2874ac
Jonathon Hare <jsh2@soton.ac.uk>
2026-07-12 20:17:39 +0100

    adding utility callbacks for hooking during training at more nuanced points

---
+++ src/model_utilities/utils/__init__.py
@@ -0,0 +1,15 @@
+from model_utilities.utils.callbacks import AtTrainingEpochs, \
+    AtTrainingIterations, EveryNBatches, EveryNEpochs, at_training_epochs, \
+    at_training_iterations, \
+    every_n_batches, every_n_epochs
+
+__all__ = [
+    "AtTrainingEpochs",
+    "AtTrainingIterations",
+    "EveryNBatches",
+    "EveryNEpochs",
+    "at_training_epochs",
+    "at_training_iterations",
+    "every_n_batches",
+    "every_n_epochs",
+]
