2e643089e82e62936edd4aadd943e28a0f6379ac
Jonathon Hare <jsh2@soton.ac.uk>
2026-08-27 17:20:05 +0100

    working on imagenet loader

--- src/model_utilities/datasets/imagenet_hdf5.py
+++ src/model_utilities/datasets/imagenet_hdf5.py
@@ -11,15 +11,12 @@ class ImageNetHDF5(VisionDataset):
     def __init__(self, root, cache_size=None, transform=None, classes=None, lazy=True):
         super(ImageNetHDF5, self).__init__(root, transform=transform, target_transform=None)
 
-        if cache_size is None:
-            if "IMAGENET_CACHE_SIZE" in os.environ:
-                cache_size = int(os.environ["IMAGENET_CACHE_SIZE"])
-            else:
-                cache_size = 0
+        if cache_size is not None:
+            print("caching has been removed and is no longer supported.")
+        if not lazy:
+            print("lazy has been removed and is no longer supported.")
 
         self.dest = pickle.load(open(os.path.join(root, 'dest.p'), 'rb'))
-        self.cache = {}
-        self.cache_size = cache_size
 
         targets = sorted(list(filter(lambda f: '.hdf5' in f, os.listdir(root))))
         if classes:
@@ -35,38 +32,65 @@ class ImageNetHDF5(VisionDataset):
                     newdest.append((dest, i))
             self.dest = newdest
 
-        if not lazy:
-            self.fill_cache()
+        self._open_files = {}
+        self._owner_pid = None
 
-    def load(self, file, i):
-        with h5py.File(os.path.join(self.root, file + '.hdf5'), 'r') as f:
-            return f['data'][i]
+    def _get_dataset(self, name):
+        pid = os.getpid()
 
-    def load_all(self, file):
-        with h5py.File(os.path.join(self.root, file + '.hdf5'), 'r') as f:
-            return list(f['data'])
+        if self._owner_pid != pid:
+            self.close()
+            self._owner_pid = pid
 
-    def fill_cache(self):
-        print('Filling cache')
-        files = (f[:-5] for f in list(filter(lambda f: '.hdf5' in f, os.listdir(self.root)))[:self.cache_size])
-        for file in files:
-            self.cache[file] = self.load_all(file)
-        print('Done')
+        if name not in self._open_files:
+            handle = h5py.File(os.path.join(self.root, name + ".hdf5"),"r")
+            try:
+                dataset = handle["data"]
+            except Exception:
+                handle.close()
+                raise
 
-    def load_from_cache(self, file, i):
-        if file in self.cache:
-            return self.cache[file][i]
+            self._open_files[name] = (handle, dataset)
 
-        if len(self.cache) >= self.cache_size:
-            return self.load(file, i)
-        else:
-            self.cache[file] = self.load_all(file)
-            return self.cache[file][i]
+        return self._open_files[name][1]
+
+    def load(self, name, index):
+        return self._get_dataset(name)[index]
+
+    def close(self):
+        handles = getattr(self, "_open_files", {})
+        for handle, _ in handles.values():
+            try:
+                handle.close()
+            except Exception:
+                pass
+        handles.clear()
+        self._owner_pid = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+    def __del__(self):
+        self.close()
+
+    def __getstate__(self):
+        # don't pickle file handles
+        state = self.__dict__.copy()
+        state["_open_files"] = {}
+        state["_owner_pid"] = None
+        return state
+
+    # def load(self, file, i):
+    #     with h5py.File(os.path.join(self.root, file + '.hdf5'), 'r') as f:
+    #         return f['data'][i]
 
     def __getitem__(self, index):
         dest, i = self.dest[index]
 
-        sample = self.load_from_cache(dest, i)
+        sample = self.load(dest, i)
 
         sample = Image.open(io.BytesIO(sample))
         sample = sample.convert('RGB')
